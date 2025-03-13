be93e1ddc4ba310e9c214fec80eba82e01374984
Ethan Goetz <125984612+goete@users.noreply.github.com>
2025-03-12 21:34:27 -0700

    Create file

---
+++ 69-Sqrt(x)/69-Sqrt(x).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0){
+            return 0;
+        } else if (x == 1) {
+            return 1;
+        }
+        long bottom = 1;
+
+        while (true) {
+            if (bottom * bottom <= x && (bottom+1)*(bottom+1) > x) {
+                return (int) bottom;
+            }
+            bottom++;
+            }
+        
+    }
+}
