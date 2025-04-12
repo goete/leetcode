13b2a6abcc7886d095e242c1b48eb4f0574474b5
Ethan Goetz <125984612+goete@users.noreply.github.com>
2025-04-11 18:38:58 -0700

    Create file

---
+++ 26-RemoveDuplicatesfromSortedArray/version/java/26-RemoveDuplicatesfromSortedArray_20250411_183858.java
@@ -0,0 +1,13 @@
+// Last updated: 4/11/2025, 6:38:58 PM
+class Solution {
+     public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+        }
+}
