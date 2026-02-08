0bc3630e88594533b2b9c406c927d7f3ab65bb5f
Ethan Goetz <125984612+goete@users.noreply.github.com>
2026-02-08 00:03:54 -0700

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,8 @@
+# Last updated: 2/8/2026, 12:03:52 AM
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1]
