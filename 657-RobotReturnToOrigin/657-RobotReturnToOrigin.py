e3414e874d0b07f1b24fb9329d07e261be1bcf0d
Ethan Goetz <125984612+goete@users.noreply.github.com>
2026-02-08 00:03:38 -0700

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.py
@@ -0,0 +1,8 @@
+# Last updated: 2/8/2026, 12:03:37 AM
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')
