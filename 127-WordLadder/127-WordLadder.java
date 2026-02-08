da88d65e5b20e0cacb09c5deaee6c3c4fc69d834
Ethan Goetz <125984612+goete@users.noreply.github.com>
2026-02-08 00:03:45 -0700

    Create 127-WordLadder

---
+++ 127-WordLadder/127-WordLadder.java
@@ -0,0 +1,37 @@
+// Last updated: 2/8/2026, 12:03:43 AM
+class Solution {
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) return 0;
+        ArrayList<String> curr = options(beginWord, wordList);
+        ArrayList<String> future = new ArrayList<String>();
+        int num = 1;
+        while (!curr.contains(endWord)) {
+            while (!curr.isEmpty()) {
+                future.addAll(options(curr.remove(0), wordList));
+            }
+            wordList.removeIf(future::contains);
+            curr = future;
+            future = new ArrayList<>();
+            num++;
+            if (curr.isEmpty()) return 0;
+        }
+        return num+1;
+    }
+    
+    private ArrayList<String> options(String s, List<String> wordList) {
+        ArrayList<String> ans = new ArrayList<>();
+        for (String word : wordList) {
+            if (oneOff(s, word)) ans.add(word);
+        }
+        return ans;
+    }
+
+    private boolean oneOff(String s, String word) {
+        int num = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != word.charAt(i)) num--;
+            if (num < 0) return false;
+        }
+        return num == 0;
+    }
+}
